6d8f4715e8e438c6d42db48febc5f66f29dfe988
Ivan Ke <ckor93@gmail.com>
2024-05-18 06:28:20 +0800

    first commit, add main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+print('Hello world')
+
+""" push an existing repository from the command line
+git remote add origin https://github.com/ivanke/tea.git
+git branch -M main
+git push -u origin main """
