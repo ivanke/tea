f0adec1c41291275dd2f34351974075ee86026bd
Ivan Ke <ckor93@gmail.com>
2024-05-18 06:48:30 +0800

    commit 2

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 print('Hello world')
 
-""" push an existing repository from the command line
-git remote add origin https://github.com/ivanke/tea.git
-git branch -M main
-git push -u origin main """
+print('commit 2')
